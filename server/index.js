5465011d0a6edc6d2fa1bd625cd2cb3eba01791e
aniketh113 <59177621+aniketh113@users.noreply.github.com>
2023-12-04 13:49:32 -0500

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,52 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+
+    if (req.url === '/') {
+    
+        fs.readFile(path.join(__dirname, 'public', 'index.html'),
+                    (err, content) => {
+                                    
+                                    if (err) throw err;
+                                    res.setHeader("Access-Control-Allow-Origin", "*")
+                                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                                    res.end(content);
+                        }
+              );
+     }
+    else if (req.url === '/about') {
+        
+        fs.readFile(
+            path.join(__dirname, 'public', 'about.html'),
+                    (err, content) => {
+                                    
+                                    if (err) throw err;
+                                    res.setHeader("Access-Control-Allow-Origin", "*")
+                                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                                    res.end(content);
+                        }
+              );
+     }
+    else if (req.url==='/api')
+    {
+        fs.readFile(
+            path.join(__dirname, 'public', 'db.json'),'utf-8',
+                    (err, content) => {
+                                    
+                                    if (err) throw err;
+                                    res.setHeader("Access-Control-Allow-Origin", "*")
+                                    // Please note the content-type here is application/json
+                                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                                    res.end(content);
+                        }
+              );
+    }
+    else{
+        res.end("<h1> 404 nothing is here</h1>");
+    }
+});
+
+const PORT= process.env.PORT || 8881;
+
+server.listen(PORT,()=> console.log(`Great our server is running on port ${PORT} `));
